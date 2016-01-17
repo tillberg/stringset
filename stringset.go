17bcd44ef8cf0518f6d67c91870b892db26a16c0
Dan Tillberg <dan@tillberg.us>
2016-01-17 13:54:52 -0500

    Initial commit

---
+++ stringset.go
@@ -0,0 +1,37 @@
+package stringset
+
+type StringSet struct {
+	strMap map[string]bool
+}
+
+func New(elements ...string) *StringSet {
+	s := &StringSet{}
+	s.Clear()
+	for _, el := range elements {
+		s.Add(el)
+	}
+	return s
+}
+func (s *StringSet) Len() int {
+	return len(s.strMap)
+}
+func (s *StringSet) Add(str string) {
+	s.strMap[str] = true
+}
+func (s *StringSet) Remove(str string) {
+	delete(s.strMap, str)
+}
+func (s *StringSet) Has(str string) bool {
+	_, has := s.strMap[str]
+	return has
+}
+func (s *StringSet) All() []string {
+	l := make([]string, 0, len(s.strMap))
+	for str, _ := range s.strMap {
+		l = append(l, str)
+	}
+	return l
+}
+func (s *StringSet) Clear() {
+	s.strMap = make(map[string]bool)
+}
